b2edfb19a586537140b8866f4bd546916a943fb3
Viktoriia <krjuchenkova@gmail.com>
2022-04-30 10:07:59 +0300

    project structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewspaperToolbarComponent } from './components/newspaper-toolbar/newspaper-toolbar.component';
+import { NewspaperListComponent } from './components/newspaper-list/newspaper-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewspaperToolbarComponent,
+    NewspaperListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/newspaper-list/newspaper-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newspaper-list',
+  templateUrl: './newspaper-list.component.html',
+  styleUrls: ['./newspaper-list.component.scss']
+})
+export class NewspaperListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
